84ff16006111371c0e3769a7aeb098fba49acd9b
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-07-10 21:13:03 +0530

    # 10-07-2025 Introduction to CPP

---
+++ cpp_introduction/dataTypes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //* data types
+    // integers -> int, long, long long
+    // decimal value -> float, double, double double
+    // a float type can also save integer value
+    
+    //* string
+    // string s1, s2;
+    // cin>>s1>>s2;
+
+    // cout<<s1<<s2;
+
+    //*taking in complete line
+
+    // string str_var;
+    // getline(cin,str_var);
+    // cout<<str_var;
+
+    //* character 
+    // characters are stored using single qoutes ('')
+    
+    // char ch = 's';
+    // cout<<ch; 
+    return 0;
+}
---
+++ cpp_introduction/intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //* output
+    // cout<<"Hello World!!"<<endl;
+    // cout<<"How are you doing";
+
+    //* input
+    // int x,y;
+    // cin>>y>>x;
+    // cout<<"first input: "<<x<<" second input: "<<y; 
+    return 0;
+}
