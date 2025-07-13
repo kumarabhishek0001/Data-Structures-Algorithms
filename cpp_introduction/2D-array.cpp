9165bb3673923206e5ef39bfd4a3085f783d9ba1
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-07-13 21:46:19 +0530

    1. Swtich 2. array & 2D arrays basics 3. String Basics

---
+++ cpp_introduction/2D-array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    // similar data types stored multiple time
+    int arr[5];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+
+    cout<<arr[3];
+    return 0;
+}
---
+++ cpp_introduction/strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    //string -> stored as an array
+    string s = "abhishek";
+    int len = s.size();
+
+    return 0;
+}
