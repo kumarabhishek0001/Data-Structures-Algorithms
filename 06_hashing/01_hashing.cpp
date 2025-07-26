71e2ca359506e383a29f6670e7dbf150213186c4
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-07-26 23:46:21 +0530

    add hashing implementation for counting occurrences of characters in a string

---
+++ 06_hashing/01_hashing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    
+
+    //precomputing
+    int hash[26] = {0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]-'a'] += 1;
+    }
+
+    int q;
+    cin>>q;
+    for(int i=0; i<q; i++){
+        char ch;
+        cin>>ch;
+        //fetching
+        cout<<hash[ch-'a']<<endl;
+    }
+
+
+    return 0;
+}
