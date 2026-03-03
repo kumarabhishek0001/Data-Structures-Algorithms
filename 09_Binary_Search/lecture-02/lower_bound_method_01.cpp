5114309a0bb59fd65dca531029a357fb551ec879
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-03-03 23:43:44 +0530

    Lecture-02: Lower bound implementation using Linear Search

---
+++ 09_Binary_Search/lecture-02/lower_bound_method_01.cpp
@@ -0,0 +1,25 @@
+// Lower bound implementation using linear search
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(vector <int> arr, int size, int target){
+
+    for(int i=0; i<size; i++){
+        if(arr[i]>=target){
+            return i;
+        }
+    }
+    return size;
+}
+
+int main(){
+
+    vector <int> nums = {1,2,3,3,7,8,9,9,9,11};
+    int size = nums.size();
+
+    int result = binarySearch(nums,size,15);
+    cout<<result;
+    return 0;
+}
